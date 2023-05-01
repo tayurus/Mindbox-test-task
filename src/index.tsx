acf810a723d6b39e588a7fbd963e1b1bf154376b
Yura <tayurus@gmail.com>
2023-05-01 17:52:24 +0000

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
-// @ts-ignore
-import { init as suppress } from "log-suppress";
 
 import "./index.css";
 import { Root } from "./Root";
-
-suppress(console);
 
 const rootElement = document.getElementById("root");
 
